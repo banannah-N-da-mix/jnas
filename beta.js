3faaba048247cb44d17bad540d97d4173a718abf
banannah-N-da-mix <banannah@gmail.com>
2015-06-08 14:44:44 -0700

    Update beta.js

--- beta.js
+++ beta.js
@@ -4,5 +4,5 @@ $( document ).ready(function() {
     console.log( "ready!" );
     
     $(".main-jumbo").css({'background-position': '0px 0px'}).
-    animate({"background-position-x": "0%","background-position-y": "100%"});
+    animate({"background-position-x": "0%","background-position-y": "100%"}, "slow");
 });
