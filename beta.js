c9f93cf36ba614721703c56d9b13718815ac1a17
banannah-N-da-mix <banannah@gmail.com>
2015-06-08 14:32:00 -0700

    Update beta.js

--- beta.js
+++ beta.js
@@ -2,4 +2,7 @@
 //Another Comment!!
 $( document ).ready(function() {
     console.log( "ready!" );
+    var div = $(".main-jumbo");
+    div.({"background-poistion": "0% 100%"}, "slow");
+    div.({"background-poistion": "0% 0%"}, "slow");
 });
