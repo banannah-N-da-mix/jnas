76ada8110567e527501ba1fc3c197e953502fc69
banannah-N-da-mix <banannah@gmail.com>
2015-06-08 14:36:31 -0700

    Update beta.js

--- beta.js
+++ beta.js
@@ -3,6 +3,6 @@
 $( document ).ready(function() {
     console.log( "ready!" );
     var div = $(".main-jumbo");
-    div.({"background-poistion": "0% 100%"}, "slow");
-    div.({"background-poistion": "0% 0%"}, "slow");
+    div.animate({"background-poistion": "0% 100%"}, "slow");
+    div.animate({"background-poistion": "0% 0%"}, "slow");
 });
